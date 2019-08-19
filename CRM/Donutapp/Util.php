594903bc7a3f2e89762b29ed821beb4fb5814206
Patrick Figel <pfigel@greenpeace.org>
2019-08-19 10:01:28 +0200

    Add backtrace to import error activity

--- CRM/Donutapp/Util.php
+++ CRM/Donutapp/Util.php
@@ -5,11 +5,18 @@ class CRM_Donutapp_Util {
   const IMPORT_ERROR_ACTIVITY_TYPE = 'streetimport_error';
 
   public static function createImportError($component, $message, $context = NULL) {
+    $details = '<h3>Error:</h3>
+                <pre>' . $message . "</pre>
+                <h3>Context:</h3>
+                <pre>" . print_r($context, TRUE) . '</pre>';
+    if (is_object($message) && $message instanceof Exception) {
+      $details .= '<h3>Backtrace:</h3><pre>' . $message->getTraceAsString() . '</pre>';
+    }
     $params = [
-      'activity_type_id'   => self::IMPORT_ERROR_ACTIVITY_TYPE,
-      'subject'            => 'DonutApp ' . $component . ' Error',
-      'status_id'          => 'Scheduled',
-      'details'            => '<pre>' . $message . "\n" . print_r($context, TRUE) . '</pre>',
+      'activity_type_id' => self::IMPORT_ERROR_ACTIVITY_TYPE,
+      'subject'          => 'DonutApp ' . $component . ' Error',
+      'status_id'        => 'Scheduled',
+      'details'          => $details,
     ];
     civicrm_api3('Activity', 'create', $params);
   }
