6bee69a6d34f0d4867c71fd400428cbd98c91106
B. Endres <endres@systopia.de>
2020-10-08 15:49:30 +0200

    set the right mandate status

--- CRM/Donutapp/Processor/Naturherzen/Donation.php
+++ CRM/Donutapp/Processor/Naturherzen/Donation.php
@@ -288,7 +288,7 @@ class CRM_Donutapp_Processor_Naturherzen_Donation extends CRM_Donutapp_Processor
 
       case 'swiss_direct_debit':
         $mandate_data['creditor_id'] = 3;
-        $mandate_data['status'] = 'ONHOLD';
+        $mandate_data['status'] = 'INIT';
         break;
 
       default:
