35cfeb9f82ca189c1c83a6ffc26e85e48f54eca4
B. Endres <endres@systopia.de>
2021-05-21 18:58:11 +0200

    changed auth endpoint and request format

--- CRM/Donutapp/API/Client.php
+++ CRM/Donutapp/API/Client.php
@@ -5,7 +5,7 @@ use GuzzleHttp\Psr7\Request;
 
 class CRM_Donutapp_API_Client {
   const API_ENDPOINT = 'https://app.formunauts.com/api/v2/ ';
-  const OAUTH2_ENDPOINT = 'https://donutapp.io/o/token/?grant_type=client_credentials';
+  const OAUTH2_ENDPOINT = 'https://app.formunauts.com/o/token/';
 
   static $apiEndpoint = self::API_ENDPOINT;
   static $oauth2Endpoint = self::OAUTH2_ENDPOINT;
@@ -127,7 +127,8 @@ class CRM_Donutapp_API_Client {
       $response = self::$oauth2Client->post(
         '',
         [
-          'auth' => [self::$clientId, self::$clientSecret, 'Basic']
+          'auth' => [self::$clientId, self::$clientSecret, 'Basic'],
+          'form_params' => ['grant_type' => 'client_credentials'],
         ]
       );
       $authResponse = json_decode($response->getBody());
